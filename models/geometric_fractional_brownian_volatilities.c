ee8601bece5ea49c901e8544d618db28e09bc3a8
Wes Devauld <wes@devauld.ca>
2012-08-21 20:25:27 -0600

    Added fractional brownian noise, renamed to volatilities

--- models/geometric_fractional_brownian_volatilities.c
+++ models/geometric_fractional_brownian_volatilities.c
@@ -1,19 +1,21 @@
-#include "fractional_brownian_motion.h"
+#include "geometric_fractional_brownian_motion.h"
 
-void fractional_brownian_noise(command_line_options* clo) {
+void geometric_fractional_brownian_volatilities(command_line_options* clo) {
     int sim,step;
-    int method = 0;
-    double *volatilities = (double *) calloc(clo->number_of_steps, sizeof(double));
+    double volatility;
+    int method = 1;
+
     double *fractional_brownian_samples = (double *) calloc(clo->number_of_steps, sizeof(double));
-    double *volatilities = (double *) calloc(clo->number_of_steps, sizeof(double));
-    DEBUG_MSG("Executing Hosking's algorithm to generate noise\n");
     
+    DEBUG_MSG("Executing Hosking's algorithm to generate paths\n");
+    DEBUG_MSG("Producing evolution of volatilities");
     for(sim = 0; sim < clo->number_of_simulations; sim++) {
         hosking(&(clo->n), &(clo->hurst_exponent), &(clo->end_time), &method, 
                 &(clo->seed1), &(clo->seed2), fractional_brownian_samples);
-                
+        
         for(step = 0; step < clo->number_of_steps; step++) {
-            printf("%lf\t", fractional_brownian_samples[step]);
+            
+            printf("%lf\t", geometric_fractional_brownian_samples[step]);
         }
         printf("\n");
     }
--- models/geometric_fractional_brownian_volatilities.h
+++ models/geometric_fractional_brownian_volatilities.h
@@ -5,4 +5,4 @@
 
 #define DEBUG_MSG(...) if(clo->debug) fprintf(stderr, __VA_ARGS__)
 
-void geometric_fractional_brownian_noise(command_line_options*);
+void geometric_fractional_brownian_volatilities(command_line_options*);
